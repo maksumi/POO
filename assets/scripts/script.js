45f3bfe1d99be22dc7486f5fc9e3c4877f7eff58
Maksumi <alejandrocanat@gmail.com>
2023-09-19 11:24:45 -0600

    Apertura de apuntes para la clase de OPP con la creación de una clase llamada persona

---
+++ assets/scripts/script.js
@@ -0,0 +1,38 @@
+ /*Paradigma
+Estructarado
+Instancia
+Plantilla
+
+
+Paradigma de programacion
+
+
+Paradigma = la forma de hacer algo
+
+Paradigma de programacion = la forma de programar, la forma de dar soluciones a problemas que se nos presentan.
+
+
+Paradigmas de programacion (formas de programar)
+
+    - Programacion imperativa: Basada en instrucciones detalladas a la computadora, ejecutadas en una secuencia determinada (C, C++, Java, Python, Swift, JS)
+
+    - Programacion basada en eventos: Enfocada en la gestion y respuesta de eventos (JS, C#, Python)
+
+    - Programacion declarativa: Explicar lo que necesitamos obtener, mas que como se hace (HTML, SQL)
+
+    - Programacion Orientada a Objetos: Toma ejemplos del mundo real (objetos), comprende sus caracteristicas (propiedades o atributos) y con base en su comportamiento o acciones (metodos), resolvemos problemas reales.
+
+
+*/
+
+
+// Creando mi primera clase
+
+class persona{
+    // Propiedades o atributos de mi clase de forma general
+    nombre = "";
+    apellido = "";
+    edad = 0;
+    email = "";
+    telefono = "";
+}
